87b1a5777b86658f4b1240a0c0c825f2d95457db
DefiCake <rarore92@gmail.com>
2023-07-20 12:12:46 +0200

    feat: step 1, rename methods and create new example files

--- host/src/main.rs
+++ host/src/main.rs
@@ -1,7 +1,7 @@
 // TODO: Update the name of the method loaded by the prover. E.g., if the method
-// is `multiply`, replace `METHOD_NAME_ELF` with `JSON_ELF` and replace
-// `METHOD_NAME_ID` with `JSON_ID`
-use json_methods::{ JSON_ELF, JSON_ID };
+// is `multiply`, replace `METHOD_NAME_ELF` with `JSON_COMPARE_ELF` and replace
+// `METHOD_NAME_ID` with `JSON_COMPARE_ID`
+use json_compare_methods::{ JSON_COMPARE_ELF, JSON_COMPARE_ID };
 use risc0_zkvm::{ default_executor_from_elf, serde::{ from_slice, to_vec }, ExecutorEnv };
 use clap::Parser;
 use std::io::{ BufReader, Read };
@@ -23,7 +23,7 @@ fn main() {
     let env = ExecutorEnv::builder().add_input(&to_vec(&data).unwrap()).build().unwrap();
 
     // Next, we make an executor, loading the (renamed) ELF binary.
-    let mut exec = default_executor_from_elf(env, JSON_ELF).unwrap();
+    let mut exec = default_executor_from_elf(env, JSON_COMPARE_ELF).unwrap();
 
     // Run the executor to produce a session.
     let session = exec.run().unwrap();
@@ -33,7 +33,7 @@ fn main() {
 
     // Optional: Verify receipt to confirm that recipients will also be able to
     // verify your receipt
-    receipt.verify(JSON_ID).unwrap();
+    receipt.verify(JSON_COMPARE_ID).unwrap();
 
     // We can extract the output of the journal
     let out: Outputs = from_slice(&receipt.journal).unwrap();
